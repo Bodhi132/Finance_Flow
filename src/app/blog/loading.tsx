7fb26afe01a4fd22c03a4b70a32464ae1661bc83
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2024-08-24 00:39:15 +0530

    loading screen modified

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -1,8 +1,8 @@
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return (
-      <div className="min-w-full min-h-full bg-blue-400">
-        <p>Loading......</p>
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+      <div className="w-screen h-screen bg-[#010D50]">
+          <p className="w-full h-full text-center font-bold text-white text-[3xl] mt-[3rem]">Loading......</p>
       </div>
-    )
-  }
+  )
+}
